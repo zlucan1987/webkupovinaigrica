33014ed4d7312adfa4f939796294491c096f3760
Zeljko Lucan <lucko1987vk@gmail.com>
2024-12-03 21:05:27 +0100

    zavrsni rad Web kupovina igrica

--- zavrsni rad Web kupovina igrica.sql
+++ zavrsni rad Web kupovina igrica.sql
@@ -11,14 +11,15 @@ create table proizvodi(
 sifra int not null primary key identity(1,1), 
 nazivigre varchar(100) not null, 
 cijena decimal (18,2) not null, 
-datumkupnje datetime
+datumkupnje datetime,
 );
 
-create table stavke (
-racun int not null references racuni(sifra),
-proizvod int not null references proizvodi(sifra),
-kolicina varchar (100) not null, 
-cijena decimal (18,2) not null 
+create table kupci(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(100) not null,
+ulica varchar(100) not null,
+mjesto varchar(100) not null
 );
 
 create table racuni(
@@ -27,10 +28,9 @@ datum datetime not null,
 kupac int not null references kupci(sifra)
 );
 
-create table kupci(
-sifra int not null primary key identity(1,1),
-ime varchar(50) not null,
-prezime varchar(50) not null,
-ulica varchar(50) not null,
-mjesto varchar(50) not null
+create table stavke(
+racun int not null references racuni(sifra),
+proizvod int not null references proizvodi(sifra),
+kolicina varchar (100) not null, 
+cijena decimal (18,2) not null 
 );
