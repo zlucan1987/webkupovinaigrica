296419394fa912565e5962be8fa7fb7b0bb7d7f8
Zeljko Lucan <lucko1987vk@gmail.com>
2025-02-12 19:46:31 +0100

    Update #003

--- Backend/Controllers/KupciController.cs
+++ Backend/Controllers/KupciController.cs
@@ -9,8 +9,6 @@ namespace Backend.Controllers
 
     public class KupciController : ControllerBase
     {
-
-
         private readonly BackendContext _context;
 
         public KupciController(BackendContext context)
@@ -21,14 +19,104 @@ namespace Backend.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            try 
-            { 
+            try
+            {
                 return Ok(_context.Kupci);
             }
             catch (Exception e)
             {
                 return BadRequest(e);
             }
+        }
+
+        [HttpGet("{sifra:int}")]
+        public IActionResult Get(int sifra)
+        {
+            if (sifra <= 0)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new { poruka = "Šifra mora biti pozitivan broj" });
+            }
+            try
+            {
+                var smjer = _context.Kupci.Find(sifra);
+                if (smjer == null)
+                {
+                    return NotFound(new { poruka = $"Smjer s šifrom {sifra} ne postoji" });
+                }
+                return Ok(smjer);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Kupac kupac) // Change parameter type to Kupac
+        {
+            try
+            {
+                _context.Kupci.Add(kupac);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, kupac);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPut("{sifra:int}")]
+        public IActionResult Put(int sifra, Kupac smjer)
+        {
+            try
+            {
+
+                var kupciBaza = _context.Kupci.Find(sifra);
+                if (kupciBaza == null)
+                {
+                    return NotFound(new { poruka = $"Kupac s šifrom {sifra} ne postoji" });
+                }
+
+                // rucni mapping - kasnije automatika
+                kupciBaza.Ime = smjer.Ime;
+                kupciBaza.Prezime = smjer.Prezime;
+                kupciBaza.Ulica = smjer.Ulica;
+                kupciBaza.Mjesto = smjer.Mjesto;
+
+                _context.Kupci.Update(kupciBaza);
+                _context.SaveChanges();
+                return Ok(kupciBaza);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpDelete("{sifra:int}")]
+        public IActionResult Delete(int sifra)
+        {
+            if (sifra <= 0)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new { poruka = "Šifra mora biti pozitivan broj" });
+            }
+            try
+            {
+                var smjer = _context.Kupci.Find(sifra);
+                if (smjer == null)
+                {
+                    return NotFound(new { poruka = $"Kupci s šifrom {sifra} ne postoji" });
+                }
+                _context.Kupci.Remove(smjer);
+                _context.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+
         }
     }
 }
